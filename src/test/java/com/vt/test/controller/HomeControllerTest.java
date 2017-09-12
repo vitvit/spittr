1e2137a6eccb4efb2cf281c30255c4b085f374ea
vitaliyt <vitaliyt@LOCUS.dev.com.ua>
2017-09-12 17:35:00 +0300

    Created css file. Added simple test for controller.

---
+++ src/test/java/com/vt/test/controller/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.vt.test.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.vt.spring.controller.HomeController;
+
+import static
+org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static
+org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+public class HomeControllerTest {
+
+	private MockMvc mock;
+	
+	@Before
+	public void setup(){
+		mock = standaloneSetup(new HomeController()).build();
+	}
+	
+	@Test
+	public void testHome() throws Exception{
+		mock.perform(get("/", "/home"))
+			.andExpect(view().name("home"));
+	}
+}
