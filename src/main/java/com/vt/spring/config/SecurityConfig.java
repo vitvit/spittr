b06d517c18baade20bd52dbc78967c2c364fd85f
bang <bang@192.168.0.101>
2017-09-18 23:25:21 +0300

    Implemented base security.

--- src/main/java/com/vt/spring/config/SecurityConfig.java
+++ src/main/java/com/vt/spring/config/SecurityConfig.java
@@ -1,11 +1,75 @@
 package com.vt.spring.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.vt.spring.repository.SpitterRepository;
+
+import security.SpitterUserService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	public static final String DEF_USER_BY_USERNAME_QUERY = 
+			"SELECT username, password, enabled " +
+			"FROM user " +
+			"WHERE username = ?";
+	public static final String DEF_AUTHORITIES_BY_USERNAME_QUERY =
+	        "select username,authority " +
+	        "from authority " +
+	        "where username = ?";
+	public static final String DEF_GROUP_AUTHORITIES_BY_USERNAME_QUERY =
+	        "select g.id, g.group_name, ga.authority " +
+	        "from group g, group_member gm, group_authority ga " +
+	        "where gm.username = ? " +
+	        "and g.id = ga.group_id " +
+	        "and g.id = gm.group_id";
+	
+	@Autowired
+	DataSource dataSource;
+	@Autowired
+	SpitterRepository spitterRepository;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		//in memory auth
+		/*
+		auth.inMemoryAuthentication()
+			.withUser("user").password("password").authorities("ROLE_USER").and()
+			.withUser("admin").password("password").authorities("ROLE_USER", "ROLE_ADMIN");
+		*/
+		//datasource auth
+		/*
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.authoritiesByUsernameQuery("SELECT username, password, true " +
+										"FROM Spitter " +
+										"WHERE username = ?")
+			.authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' " +
+										"FROM Spitter where username=?")
+			.passwordEncoder(new BCryptPasswordEncoder());
+		*/
+		//LDAP auth
+		/*
+		auth.ldapAuthentication().userSearchBase("ou=peaople")
+								 .userSearchFilter("(uid={0})")
+								 .groupSearchBase("groups")
+								 .groupSearchFilter("group={0}")
+								 //.passwordCompare()
+								 //.passwordEncoder(new Md5PasswordEncoder())
+								 //.passwordAttribute("passcode")
+								 .contextSource()
+								 	.url("ldap://testldap.com:389/dc=testldap,dc=com");
+		*/
+		//custom user service
+		auth.userDetailsService(new SpitterUserService(spitterRepository));
+	}
 }
---
+++ src/main/java/security/SpitterUserService.java
@@ -0,0 +1,38 @@
+package security;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.vt.spring.domain.Spitter;
+import com.vt.spring.repository.SpitterRepository;
+
+public class SpitterUserService implements UserDetailsService {
+
+	private final SpitterRepository spitterRepository;
+	
+	public SpitterUserService(SpitterRepository spitterRepository) {
+		this.spitterRepository = spitterRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Spitter spitter = spitterRepository.findByUsername(username);
+		
+		if(spitter != null){
+			List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			authorities.add(new SimpleGrantedAuthority("ROLE_SPITTER"));
+		
+			return new User(spitter.getUsername(), spitter.getPassword(), authorities);
+		}
+		
+		throw new UsernameNotFoundException("User " + username + " not found.");
+	}
+}
