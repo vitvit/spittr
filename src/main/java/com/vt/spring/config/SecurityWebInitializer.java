bb677b106b1dab836fe51ae5adfe8bdc6810f85a
vitaliyt <vitaliyt@LOCUS.dev.com.ua>
2017-09-18 18:40:44 +0300

    Added security config classes.

---
+++ src/main/java/com/vt/spring/config/SecurityWebInitializer.java
@@ -0,0 +1,7 @@
+package com.vt.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
