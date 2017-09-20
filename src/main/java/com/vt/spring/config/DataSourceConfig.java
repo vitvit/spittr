b5975a9f166a7d6b7e3ab35f51387e6cbccbb992
bang <bang@192.168.0.101>
2017-09-21 00:19:12 +0300

    Fix for register form. Registered jdbc template.

--- src/main/java/com/vt/spring/config/DataSourceConfig.java
+++ src/main/java/com/vt/spring/config/DataSourceConfig.java
@@ -6,6 +6,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jndi.JndiObjectFactoryBean;
@@ -44,4 +45,9 @@ public class DataSourceConfig {
 		jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
 		return (DataSource) jndiObjectFactoryBean.getObject();
 	}
+	
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+      return new JdbcTemplate(dataSource);
+    }
 }
